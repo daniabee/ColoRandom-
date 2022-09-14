bcadcd524527c4a8bc20e26f707c491301862756
Adelle Pitsas <apitsas4@gmail.com>
2022-09-14 17:54:38 -0400

    refactored palette class to restart with empty array on replace array and cleaned up code

--- scripts.js
+++ scripts.js
@@ -1,7 +1,5 @@
-var currentColors = document.querySelector('.palettes_current')
 var newPaletteButton = document.querySelector('.buttons_new-palette')
 // Golbal Variables
-var previouslyLockedColors = [];
 var newPalette = new Palette
 
 
