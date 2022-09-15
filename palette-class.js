3e31f001561b20aa9b8fb20adc7b525b995989cd
Dani Bagley <bagley.a.danielle@gmail.com>
2022-09-15 12:00:19 -0600

    Add almost working saved section. Breaks on third palette.

--- palette-class.js
+++ palette-class.js
@@ -5,16 +5,35 @@ class Palette {
 
   }
 
-  replaceColor(color) {
+  replaceColor() {
     this.colors.length = 0;
-    var unlockedColors = document.querySelectorAll('.unlocked');
-    var lockedColors = document.querySelectorAll('.locked');
-    for (var i = 0; i < unlockedColors.length; i++) {
-      var newColor = new Color();
-        unlockedColors[i].style.backgroundColor = `#${newColor.hex}`
-        this.colors.push(newColor);
+    var currentPalette = document.querySelectorAll('.palettes_current');
+    var hexCode = document.querySelectorAll('.hex-code');
+    for (var i = 0; i < currentPalette.length; i++) {
+      var newColor = new Color ();
+      if (currentPalette[i].classList.contains('unlocked')) {
+        currentPalette[i].style.backgroundColor = `#${newColor.hex}`
+      }
+      else if (currentPalette[i].classList.contains('locked')) {
+        newColor.hex = currentPalette[i].style.backgroundColor;
+      }
+      this.colors.push(newColor);
+    }
+    for (var i = 0; i < hexCode.length; i++) {
+      hexCode[i].innerText = `#${this.colors[i].hex}`;
     }
   }
+
+  // replaceColor(color) {
+  //   this.colors.length = 0;
+  //   var unlockedColors = document.querySelectorAll('.unlocked');
+  //   var lockedColors = document.querySelectorAll('.locked');
+  //   for (var i = 0; i < unlockedColors.length; i++) {
+  //     var newColor = new Color();
+  //       unlockedColors[i].style.backgroundColor = `#${newColor.hex}`
+  //       this.colors.push(newColor);
+  //   }
+  // }
 
   lockColor(selectedColor) {
     this.colors[selectedColor].locked = true;
--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ var newPaletteButton = document.querySelector('.buttons_new-palette')
 var saveButton = document.querySelector('.buttons_save-palette');
 var miniSavedPalettes = document.querySelector('.mini_palette');
 // Golbal Variables
-var newPalette = new Palette
+var newPalette = new Palette();
 var savedPalettes = [];
 
 
@@ -16,24 +16,44 @@ function generateNewPalette() {
 }
 
 function savePalette() {
-  savedPalettes.unshift(newPalette.colors);
-  // miniSavedPalettes.innerHTML = ``;
+  // savedPalettes = [];
   for (var i = 0; i < newPalette.colors.length; i++) {
+    savedPalettes.push(`#${newPalette.colors[i].hex}`);
+  }
     miniSavedPalettes.innerHTML += `
 
-    <section class="palettes_color" data-index="0">
-      <section class="palettes_current locked mini" data-index="0"></section>
-      <section class="palettes_hex-key" data-index="0">
+    <section class="mini_palette">
+        <section class="palettes_color" data-index="0">
+          <section class="palettes_current mini locked" data-index="0"></section>
+          <section class="palettes_hex-key" data-index="0">
+          </section>
+        </section>
+          <section class="palettes_color" data-index="1">
+          <section class="palettes_current mini locked" data-index="1"></section>
+          <section class="palettes_hex-key" data-index="1">
+        </section>
+        </section>
+        <section class="palettes_color" data-index="2">
+          <section class="palettes_current mini locked" data-index="2"></section>
+          <section class="palettes_hex-key" data-index="2">
+          </section>
+        </section>
+          <section class="palettes_color" data-index="3">
+          <section class="palettes_current mini locked" data-index="3"></section>
+          <section class="palettes_hex-key" data-index="3">
+        </section>
+        </section>
+          <section class="palettes_color" data-index="4">
+          <section class="palettes_current mini locked" data-index="4"></section>
+          <section class="palettes_hex-key" data-index="4">
+          </section>
+        </section>
     </section>
     `
-  }
-  var miniBox = document.querySelectorAll('.mini');
+    var currentPaletteMini = document.querySelectorAll('.mini');
+    var hexCode = document.querySelectorAll('.hex-code');
+    for (var i = 0; i < currentPaletteMini.length; i++) {
+      currentPaletteMini[i].style.backgroundColor = `${savedPalettes[i]}`
+    }
 
-  for (var i = 0; i < miniBox.length; i++) {
-    miniBox[i].style.backgroundColor = `#${savedPalettes[0][i].hex}`
-  }
-
-
-  // box[i].style.backgroundColor = `#${savedPalettes[0][i]}`
-  // unlockedColors[i].style.backgroundColor = `#${newColor.hex}`
 }
