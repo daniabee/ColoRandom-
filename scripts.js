24c9ae62a9e832c36b8dbed59b5e26b190e7c259
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-09-17 15:36:35 -0400

    clean up some code

--- scripts.js
+++ scripts.js
@@ -41,6 +41,7 @@ function generateNewPalette() {
 function savePalette() {
   var saveThisPalette = new Palette(mainPalette.colors);
   savedPalettes.push(saveThisPalette);
+  // create a function seperating data model from dom below 
   miniSavedPalettes.innerHTML = '';
 
 
@@ -59,10 +60,21 @@ function savePalette() {
     miniSavedPalettes.innerHTML += `
       <section class="saved-palette-container">
         ${pallet}
-        <img class= "mini_trashCan" src="./imgFolder/trash-can.png" alt="trash can icon">
+        <img class= "mini_trashCan" src="./imgFolder/trash-can.png" alt="trash can icon" key=${savedPalettes[i].id}>
       </section>
     `
   }
+
+  //add a value/key to trash can img
+  // being created as part of original for loop 47
+  // savepalet.i.// ID
+  // put eventlisterner on big container
+  // target (function )( function (event))
+  //function will hit if( event.target.value.classList.
+  // does even.t.target.key === date.object // instanceof// update data model (remove from array )// maybe splice method...
+  // function renders// the dom with a function
+  //does event.target.contains(mini_trashCan
+  // queryselector (console.log() frequently)
   mainPalette.replaceColor();
 };
 
